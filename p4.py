34436f5e2a51a34356d464ad852a83ebd82f2afe
chaitanya19soni <1839chhaitanya@gmail.com>
2026-04-23 23:35:19 +0530

    p4

---
+++ p4.py
@@ -0,0 +1,16 @@
+marks = list(map(int ,input().split))
+total = sum (marks)
+per = sum(marks)/len(marks)
+
+print (total)
+
+if per > 75:
+	print("Distinction")
+elif per >= 60:
+	print("First Division")
+elif per >= 50:
+	print("Second Division")
+elif per >= 40:
+	print("Third Division")
+else:
+	print("Fail")
