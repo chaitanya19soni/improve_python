903ecc43c587ce88e58aefa55816ff9e3d6c40b1
chaitanya19soni <1839chhaitanya@gmail.com>
2026-05-01 23:04:19 +0530

    fibonachi

---
+++ py5.py
@@ -0,0 +1,13 @@
+def fibo (num):
+    sum = 0
+    n1,n2 = 0 ,1 
+    while num>sum: 
+        print(sum)
+        n1 = n2 
+        n2 = sum 
+        sum = n1+n2
+num  = int (input ("enter the number"))
+fibo(num)
+
+
+
