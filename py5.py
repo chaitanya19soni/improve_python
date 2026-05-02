c00474d2888b65df39f9f6591d23c7204c5dd218
chaitanya19soni <1839chhaitanya@gmail.com>
2026-05-02 20:29:01 +0530

    py

--- py5.py
+++ py5.py
@@ -1,13 +1,9 @@
 def fibo (num):
     sum = 0
-    n1,n2 = 0 ,1 
-    while num>sum: 
-        print(sum)
+    n1,n2 = 0 ,1
+    while(num >sum):
         n1 = n2 
         n2 = sum 
         sum = n1+n2
-num  = int (input ("enter the number"))
+num =  int (input ())
 fibo(num)
-
-
-
